854dcbdfb87c941e11566484b6d5d924bb24d016
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-01-17 13:58:30 +0000

    Storing Original Data

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
     console.log('constructor');
   }
@@ -31,6 +32,11 @@ class App extends Component {
 
   render() {
     console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return ( 
       <div className='App'>
         <input 
@@ -38,18 +44,15 @@ class App extends Component {
           type='search' 
           placeholder='search monsters' 
           onChange={(event) => {
-            console.log(event.target.value);
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
-
-            this.setState(() => {
-              return { monsters: filteredMonsters };
-            })
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(
+              () => {
+                return { searchField };
+              }
+            );
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
             return ( 
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
