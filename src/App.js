8cf63d7e175f42c39b0302cf597713d20e609677
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-01-17 13:06:41 +0000

    Input Search Box Component

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
-    console.log('1');
+    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('3');
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => 
         this.setState(
           () => {
-            return {monsters: users};
+            return { monsters: users };
           },
           () => {
             console.log(this.state);
@@ -30,11 +30,18 @@ class App extends Component {
   }
 
   render() {
-    console.log('2');
+    console.log('render');
     return ( 
-      <div className="App">
-        {
-          this.state.monsters.map((monster) => {
+      <div className='App'>
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        {this.state.monsters.map((monster) => {
             return ( 
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
