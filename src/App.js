139ca710bf73e3d959114b746fb72b62cfb8b732
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-01-19 14:53:37 +0000

    DOM Paint Flashing

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
-  const [searchField, setSearchField] = useState('a'); // [value, setValue]
+  const [searchField, setSearchField] = useState(''); // [value, setValue]
+  const [title, setTitle] = useState('');
   const[monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilterMonsters] = useState(monsters);
-
-  console.log('rendered');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -27,17 +26,27 @@ const App = () => {
  
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);  
+  };
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);  
   };
 
   return (
     <div className='App'>
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">{title}</h1>
 
       <SearchBox 
         className='monsters-search-box'
         onChangeHandler={onSearchChange} 
         placeholder='search monsters' 
+      />
+      <br/>
+      <SearchBox 
+        className='title-search-box'
+        onChangeHandler={onTitleChange} 
+        placeholder='set title' 
       />
       <CardList monsters={filteredMonsters} />
     </div>
